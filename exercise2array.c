991b4cb935ad28d19a5c4a3df15ecd61b0b626cf
WongChinWee <93850449+WongChinWee@users.noreply.github.com>
2022-05-28 11:58:34 +0800

    Add files via upload

---
+++ exercise2array.c
@@ -0,0 +1,26 @@
+//documentation section 
+/* Exercise 2 - 1D array */
+
+//pre-processor section 
+#include<stdio.h>
+
+//global variable section 
+
+
+//main function section 
+int main(){
+    char myName[100] = {"Wong"};
+    
+    
+    //write here your program
+
+    printf("My Name is %s, Bachelor of Information Technology from Universiti Malaysia Kelantan", myName);
+    
+
+
+    return 0;
+}
+
+
+//user-defined section
+
